3eb86286b0e9fb2f522f9d3181aeaa51730ac61e
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2019-07-31 13:03:20 +0200

    Add medium simulation configs

--- plot_all_abb.sh
+++ plot_all_abb.sh
@@ -4,10 +4,14 @@ folder_name=$1
 
 mkdir -p ${folder_name}
 
-for p in total_runtime best_integer_cost max_edge_load max_node_load;
+for exid in 0 1;
 do
-    python -m evaluation_fog_model_2019.cli make_box_plot $2 "substrate_generation/substrates/ABBUseCaseFogNetworkGenerator/pseudo_random_seed"\
-            "substrate_generation/substrates/ABBUseCaseFogNetworkGenerator/sensor_actuator_loop_count"\
-            $p --output_plot_file_name "N-to-$p" --output_path `pwd`/$1 --show_feasibility
-        for file in ${ALIB_EXPERIMENT_HOME}/log/*; 	do mv $file ${folder_name} ; done
+    for p in total_runtime best_fractional_cost best_integer_cost max_edge_load \
+              max_node_load preprocess_runtime optimization_runtime postprocess_runtime;
+    do
+        python -m evaluation_fog_model_2019.cli make_box_plot $2 "substrate_generation/substrates/ABBUseCaseFogNetworkGenerator/pseudo_random_seed"\
+                "substrate_generation/substrates/ABBUseCaseFogNetworkGenerator/sensor_actuator_loop_count"\
+                $p --output_plot_file_name "N-to-$p-execution-${exid}" --output_path `pwd`/$1 --show_feasibility --execution_id ${exid} ;
+            for file in ${ALIB_EXPERIMENT_HOME}/log/*; 	do mv $file ${folder_name} ; done
+    done
 done
